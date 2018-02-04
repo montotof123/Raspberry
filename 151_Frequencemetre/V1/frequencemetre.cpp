a30f82264a421b52e346c639de729fb0047089c4
montotof123 <christophe.sinclair@orange.fr>
2018-02-04 19:25:56 +0100

    Add files via upload

---
+++ 151_Frequencemetre/V1/frequencemetre.cpp
@@ -0,0 +1,71 @@
+﻿//================================================
+// Name        : fréquencemètre.cpp
+// Author      : totof
+// Version     :
+// Copyright   : Free
+// Description : Mesurer une fréquence sur un GPIO
+//================================================
+
+#include <iostream>	  // impression cout
+#include <stdlib.h>   // La librairie standard
+#include <signal.h>	  // Fin
+#include <chrono>     // Chronometre
+#include "GestionPinGPIO.h"
+
+using namespace std;
+using namespace std::chrono;
+
+// Objets utilisés
+GestionPinGPIO mesure(GPIO26);
+
+// Horloge
+high_resolution_clock::time_point horloge;
+high_resolution_clock::time_point svgHorloge;
+duration<double> time_span;
+
+// *************************************
+// Fonction de fin déclenchée par CTRL-C
+// *************************************
+void fin(int sig)
+{
+    // Désactive les GPIO
+    mesure.~GestionPinGPIO();
+    cout << "FIN signal: " << sig << endl;
+    exit(0);
+}
+
+// -----------------------
+// Fonction d'interruption
+// -----------------------
+void fctInterrupt(void) {
+	float frequence; 
+	
+	// Calcul de la vitesse de rotation
+	horloge = high_resolution_clock::now();
+	time_span = duration_cast<chrono::duration<double>>(horloge - svgHorloge);
+	if(time_span.count() != 0) {
+		frequence = 1 / time_span.count();
+	}
+	
+	// Affichage et mémorisation
+	cout << "Fréquence: " << frequence << " Hz" << endl;
+	svgHorloge = horloge;
+}
+
+
+// ******************
+// Fonction pricipale
+// ******************
+int main() {
+    // Ecoute du CTRL-C avec fonction à lancer
+    signal(SIGINT, fin);
+
+	// Programmation de la reception
+	mesure.in();
+	mesure.pullUp();
+	mesure.fctInterrupt(Falling, &fctInterrupt);
+	
+	for(;;) {
+		delay(500);
+	}
+}
---
+++ 151_Frequencemetre/V2/frequencemetre.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var fs = require('fs');
+var Gpio = require('onoff').Gpio;
+var hrtime = require('process.hrtime');
+
+// Parametrage du gpio
+var pushButton = new Gpio(26, 'in', 'falling');
+
+var horloge;
+var svgHorloge = hrtime();
+var frequence;
+
+// Création du serveur web avec chargement du fichier led.html affiché au client
+// Ces une application monopage, le serveur affichera toujours cette page
+//    sauf pour les javascript et les images qui sont traités à part
+var server = http.createServer(function(req, res) {
+	console.log(`${req.method} ${req.url}`);
+	fs.readFile('./frequencemetre.html', 'utf-8', function(error, content) {
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end(content);
+	});
+});
+var io = require('socket.io').listen(server);
+
+// Gestion des messages
+io.sockets.on('connect', function (socket) {
+	// Message de connexion sur la console et vers le client
+	console.log('Un client est connecté !');
+});
+
+// Gestion de l'interruption si activation du bouton
+// Message sur la console, allumage de la led et envoie d'un message au client
+pushButton.watch(function(delta) {
+	horloge = hrtime();
+	frequence = 1 / ((horloge[0] + horloge[1] / 1e9) - (svgHorloge[0] + svgHorloge[1] / 1e9));
+	io.emit('affiche', Math.trunc(frequence));
+	svgHorloge[0] = horloge[0];
+	svgHorloge[1] = horloge[1];
+});
+
+// Mise en attente des connections vers le serveur Web
+server.listen(8080);
