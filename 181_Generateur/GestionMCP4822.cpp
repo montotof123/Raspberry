07e7c8ef88293cc4f568c06fbecf1ac75cee8e67
montotof123 <christophe.sinclair@orange.fr>
2018-02-20 21:19:03 +0100

    Add files via upload

---
+++ 181_Generateur/GestionMCP4822.cpp
@@ -0,0 +1,143 @@
+/*
+ * GestionMCP4822.cpp
+ *
+ *  Created on:21 octobre 2016
+ *      Author: totof
+ * Controle un MCP4822 avec la librairie wiringPi sur Raspberry Pi
+*/
+ 
+// ****************************************************
+// Attention: la commande wiringPiSPIDataRW passe les
+// octets dans l'ordre 0,1,2 etc, il faut donc inverser
+// les octets 0 et 1 dans toutes les commandes 16 bits
+// ainsi que dans les résultats
+// ****************************************************
+
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <stdint.h>
+#include <linux/spi/spidev.h>
+
+#include "GestionMCP4822.h"
+#include "GestionSPI.h"
+
+// *****************************************
+// Constructeur
+// @param pChannel le canal de communication
+// Mémorise le canal et initialise le SPI
+// *****************************************
+GestionMCP4822::GestionMCP4822(Channel pChannel) {
+	channel = pChannel;
+	Frequence frequence = speed_50;
+	wiringPiSPISetup (channel, frequence);
+}
+
+// ****************************************************
+// Constructeur
+// @param pChannel le canal de communication
+// @param pFrequence la frequence
+// Mémorise le canal, la frequence et initialise le SPI
+// ****************************************************
+GestionMCP4822::GestionMCP4822(Channel pChannel, Frequence pFrequence) {
+	channel = pChannel;
+	wiringPiSPISetup (channel, pFrequence);
+}
+
+// ***********************************************
+// Ecrit une valeur dans les registres 
+// @param pDac le DAC
+// @param pValue la valeur
+// @param pGain le gain
+// @param pSHDN le SHDN
+// @return true si OK, false si valeur trop grande
+// ***********************************************
+bool GestionMCP4822::write(Dac pDac, unsigned short pValue, Gain pGain = FOIS_2, SHDN pSHDN = NO) {
+	// Erreur valeur trop grande
+	if(pValue > MAX_VALUE) {
+		return false;
+	}
+	
+	union spiData data;
+	data.uSData = pDac | pValue | pGain | pSHDN;
+	swap(data);
+	wiringPiSPIDataRW (channel, data.uCData, 2);
+	return true;
+}
+
+// ***********************************************
+// Demande une tension precise à un DAC
+// @param pDac le DAC
+// @param pValue la valeur
+// @return true si OK, false si valeur trop grande
+// ***********************************************
+bool GestionMCP4822::output(Dac pDac, float pValue){
+	SHDN shutdown;
+	unsigned short value;
+	Gain gain;
+	
+	// Pas de valeur negative
+	if(pValue < 0) {
+		return false;
+	}
+	
+	// Entre 0 et 2,048V
+	if (pValue < MID_VALUE) {
+		shutdown = NO;
+		gain = FOIS_1;
+		value = pValue * 1000.0 * 2;
+	}
+	// Entre 2,048V et 4,09599...V
+	if (pValue >= MID_VALUE) {
+		shutdown = NO;
+		gain = FOIS_2;
+		value = pValue * 1000.0;
+	}
+	// Au dessus de 4.096V (valeur trop grande)
+	if (pValue > MAX_VALUE / 1000.0) {
+		shutdown = NO;
+		gain = FOIS_1;
+		value = 0xFFFF;
+	}
+	// Haute impedance
+	if(pValue == HIGH_IMPEDANCE) {
+		shutdown = YES;
+		value = MAX_VALUE;
+		gain = FOIS_1;
+	}
+	return write(pDac, value, gain, shutdown);
+}
+
+// *********************
+// Donne la valeur max
+// @return la valeur max
+// *********************
+float GestionMCP4822::getMaxValue(){
+	return MAX_VALUE / 1000.0;
+}
+
+// ************************************
+// Donne la valeur de haute impedance
+// @return la valeur de haute impedance
+// ************************************
+float GestionMCP4822::getHighImpedance(){
+	return HIGH_IMPEDANCE;
+}
+
+// *********************************************
+// swap les deux octets d'un spiData
+// @param un pointeur sur la structure à swapper
+// *********************************************
+void GestionMCP4822::swap(spiData &data) {
+	unsigned char temp = data.uCData[0];
+	data.uCData[0] = data.uCData[1];
+	data.uCData[1] = temp;
+}
+
+// ***********
+// Destructeur
+// ***********
+GestionMCP4822::~GestionMCP4822() {
+	output(A, HIGH_IMPEDANCE);
+	output(B, HIGH_IMPEDANCE);
+}
+
---
+++ 181_Generateur/generateur.cpp
@@ -0,0 +1,130 @@
+﻿//=============================================================
+// Name        : generateur.cpp
+// Author      : totof
+// Version     :
+// Copyright   : Free
+// Description : Generateur de signaux carre, triangle ou sinus
+//=============================================================
+
+#include <iostream>	  // impression cout
+#include <stdlib.h>   // La librairie standard
+#include <signal.h>	  // Fin, fonction cyclique
+#include <unistd.h>	  // Delais pour les alarmes
+#include <string.h>	  // String
+#include <wiringPi.h>
+#include "GestionMCP4822.h"
+#include "LibSignal.h"
+
+#define NOSIGNAL 0
+#define CARRE 1
+#define TRIANGLE 2
+#define SINUS 3
+
+using namespace std;
+
+// Objets utilisés
+GestionMCP4822 mcp4822(channel_0);
+int valSignal = NOSIGNAL;
+float tensionMax;
+int position = 0;
+Dac canal;
+
+// *************************************
+// Fonction de fin déclenchée par CTRL-C
+// *************************************
+void fin(int sig) {
+    // Désactive le generateur
+    mcp4822.~GestionMCP4822();
+    cout << "FIN signal: " << sig << endl;
+    exit(0);
+}
+
+// ****************
+// Generateur carre
+// ****************
+void genCarre(int sig) {
+	mcp4822.output(canal, carre[position] * tensionMax);
+	position = (position + 1) % 2;
+}
+
+// *******************
+// Generateur triangle
+// *******************
+void genTriangle(int sig) {
+	mcp4822.output(canal, triangle[position] * tensionMax);
+	position = (position + 1) % 20;
+}
+
+// ****************
+// Generateur sinus
+// ****************
+void genSinus(int sig) {
+	mcp4822.output(canal, sinus[position] * tensionMax);
+	position = (position + 1) % 40;
+}
+
+// ******************
+// Fonction pricipale
+// ******************
+int main(int argc, char *argv[]) {
+	if(argc != 5) {
+		cout << "Fonctionnement:" << endl;
+		cout << "  generateur canal signal tension interruption" << endl;
+		cout << "    canal : {A, B}" << endl;
+		cout << "    signal : {carre, triangle, sinus}" << endl;
+		cout << "    tension: tension max entre 0 et 4.095" << endl; 
+		cout << "    interruption: temps en microSeconde entre deux lectures de valeur" << endl;
+		cout << "  pour un même temps d'interruption, la fréquence variera en fonction du signal" << endl;
+		cout << "  Le carré à 2 pas" << endl;
+		cout << "  Le triangle à 20 pas" << endl;
+		cout << "  Le sinus à 40 pas" << endl;
+	}
+	
+    // Ecoute du CTRL-C avec fonction à lancer
+    signal(SIGINT, fin);
+
+	// Verification et stockage canal
+	bool correctCanal = false;
+	if(!strcmp(argv[1], "A")) {
+		canal = A;
+		correctCanal = true;
+	}
+	if(!strcmp(argv[1], "B")) {
+		canal = B;
+		correctCanal = true;
+	}
+	if(!correctCanal) {
+		exit(1);
+	}
+	
+	// Verification et stockage tension
+	tensionMax = atof(argv[3]);
+	if(tensionMax > 4.095) {
+		exit(3);
+	}
+	
+	// Verification et stockage type signal
+	if(!strcmp(argv[2], "carre")) {
+		valSignal = CARRE;
+		signal(SIGALRM, genCarre);   
+	}
+	if(!strcmp(argv[2], "triangle")) {
+		valSignal = TRIANGLE;
+		signal(SIGALRM, genTriangle);   
+	}
+	if(!strcmp(argv[2], "sinus")) {
+		valSignal = SINUS;
+		signal(SIGALRM, genSinus);   
+	}
+	if(valSignal == NOSIGNAL) {
+		exit(2);
+	}
+	
+	// Lancement cyclique d'une mesure
+    ualarm(atoi(argv[4]), atoi(argv[4]));
+
+	// Boucle d'attente infinie
+	for(;;) {
+		delay(500);
+	}
+}
