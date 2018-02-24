65c99000d3e26ae8780fecb7f9e70bd66f728874
montotof123 <christophe.sinclair@orange.fr>
2018-02-24 14:26:22 +0100

    Add files via upload

--- 140_Oscillo/oscMesure.cpp
+++ 140_Oscillo/oscMesure.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : oscVitesse.cpp
+// Name        : oscMesure.cpp
 // Author      : totof
 // Version     :
 // Copyright   : Free
-// Description : Test de vitesse du convertisseur MCP3208 pour l'oscilloscope
+// Description : Mesure du convertisseur MCP3208 pour l'oscilloscope
 //============================================================================
 
 #include <iostream>	  // impression cout
@@ -12,20 +12,24 @@
 #include <errno.h>    // Gestion des numéros d'erreur
 #include <string.h>   // Gestion des String d'erreur
 #include <wiringPi.h> // La wiringPi
+#include <chrono>     // Chronometre
 #include <fstream>
-// #include <ctime>
 #include "GestionMCP3208.h"
 
 #define NOMBRE_MESURE 300000
 
 int compteur = 0;
 float mesures[NOMBRE_MESURE];
-clock_t temps[NOMBRE_MESURE];
+unsigned int temps[NOMBRE_MESURE];
 
 using namespace std;
+using namespace std::chrono;
 
 // Objets utilisés
-GestionMCP3208 mcp3208(CHANNEL_1, SPI_SPEED_50);
+GestionMCP3208 mcp3208(CHANNEL_0, SPI_SPEED_50);
+high_resolution_clock::time_point horloge;
+high_resolution_clock::time_point initHorloge;
+duration<double> time_span;
 
 // *************************************
 // Fonction de fin déclenchée par CTRL-C
@@ -73,12 +77,15 @@ int main() {
 
 	float tension;
 	cout << "DEBUT mesure: " << clock() << endl;
+	initHorloge = high_resolution_clock::now();
 	
 	// On boucle
 	while (compteur != NOMBRE_MESURE) {
 		// Lecture entrée 7
+		horloge = high_resolution_clock::now();
+		time_span = duration_cast<chrono::duration<double>>(horloge - initHorloge);
 		mesures[compteur] = mcp3208.readTension(VOIE_7, ALIM_33);
-		temps[compteur] = clock();
+		temps[compteur] = time_span.count() * 1000000;
 		compteur++;
 	}
 	fin();
