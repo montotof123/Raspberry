e5fed1c8f69b89f6539c72783c5586867824c915
Christophe Sinclair <christophe.sinclair@orange.fr>
2020-06-21 22:51:41 +0200

    Add files via upload

---
+++ 090_web/Node.js/meteo-api/meteo-angular/src/app/page-error/page-error.component.ts
@@ -0,0 +1,19 @@
+// ****************************
+// Classe pour la page d'erreur
+// ****************************
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-error',
+  templateUrl: './page-error.component.html',
+  styleUrls: ['./page-error.component.css']
+})
+export class PageErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
